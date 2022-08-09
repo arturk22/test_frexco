35f06850d4e4243d689575db6050dd4090372e51
arturk22 <alencar.artur00@gmail.com>
2022-08-09 19:21:06 -0300

    heroku

--- src/App.js
+++ src/App.js
@@ -6,18 +6,16 @@ import './reset.css';
 import Details from './pages/Details';
 import Home from './pages/Home';
 
+class App extends Component{
 
+  render(){
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+    <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/details/:id' element={<Details />}/>
     </Routes>
   </BrowserRouter>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  }
+}
+export default App;
